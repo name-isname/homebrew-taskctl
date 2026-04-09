e66f03a497f7ff924876aab4dce41b97aa1a4bb0
nameisname <757354335@qq.com>
2026-04-09 14:24:27 +0800

    Update taskctl to v0.2.0

--- Formula/taskctl.rb
+++ Formula/taskctl.rb
@@ -5,31 +5,31 @@ class Taskctl < Formula
   desc "Process-oriented task management tool with CLI, TUI, and Web UI"
   homepage "https://github.com/name-isname/tasker"
   url "https://github.com/name-isname/tasker.git",
-      tag:      "v0.1.1",
-      revision: "11b226fd0c877424e5197458467d31bf5f617f92"
+      tag:      "v0.2.0",
+      revision: "e5ccfa2ff33a899f5c505fafe42bf6d32008a227"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/name-isname/tasker/releases/download/v0.1.1/tasker_0.1.1_darwin_arm64.tar.gz"
-      sha256 "4e76551b31ce880b846ebae9d3f61dba46a52d5778d93e19f696ea5c25559ff4"
+      url "https://github.com/name-isname/tasker/releases/download/v0.2.0/tasker_0.2.0_darwin_arm64.tar.gz"
+      sha256 "97c127153668a13f7c136a10ac3425cff68ad3688f2756df9f844b976ce78717"
     end
 
     on_intel do
-      url "https://github.com/name-isname/tasker/releases/download/v0.1.1/tasker_0.1.1_darwin_amd64.tar.gz"
-      sha256 "19204044e9e17bbf3be5e633a7916a368f30854d27210e837e2deb2546a722b4"
+      url "https://github.com/name-isname/tasker/releases/download/v0.2.0/tasker_0.2.0_darwin_amd64.tar.gz"
+      sha256 "3c2216152b3e8e149b7c5576291c2528caf6fff5b5bb4613795922e2d7b38a62"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/name-isname/tasker/releases/download/v0.1.1/tasker_0.1.1_linux_amd64.tar.gz"
-      sha256 "60fb794f80bf9b2f1555da76d6695bcb21d7692bfa295eec7d99586308d6419d"
+      url "https://github.com/name-isname/tasker/releases/download/v0.2.0/tasker_0.2.0_linux_amd64.tar.gz"
+      sha256 "76a852a546f5d693605a104e410c8d51a7db6c9ae227f9b7789e58ea20e6e4ec"
     end
 
     on_arm do
-      url "https://github.com/name-isname/tasker/releases/download/v0.1.1/tasker_0.1.1_linux_arm64.tar.gz"
-      sha256 "e5709451ac72aafbf1fdb30410b97ac152758abfb1b06132954090ab91a5b2e3"
+      url "https://github.com/name-isname/tasker/releases/download/v0.2.0/tasker_0.2.0_linux_arm64.tar.gz"
+      sha256 "0acc1ba90ce64c05baa6e18b49ebef551119dfc41096ae8a3ce0f80bb588de96"
     end
   end
 
